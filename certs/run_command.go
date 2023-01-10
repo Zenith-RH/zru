594e3e0a6fd033938959ddddb4786754b7053de0
winds0r <wontsleep@protonmail.com>
2023-01-10 23:01:38 +0900

    (fix/certs)closing file descriptors at the end

--- certs/run_command.go
+++ certs/run_command.go
@@ -3,9 +3,9 @@ package zru
 import (
 	"bufio"
 	"fmt"
-	"io"
+    "io"
+    "os"
 	"log"
-	"os"
 	"os/exec"
 
 	"github.com/fatih/color"
@@ -26,51 +26,54 @@ func Run(cmd *exec.Cmd, name string, isInteractive bool, path string) {
 		return
 	}
 
-	color.Cyan("Runnig command interactively. Press SPACE to detach...")
+    color.Cyan("Runnig command interactively. Press SPACE to detach...")
 
-	/* Getting output pipes */
+    /* Getting output pipes */
 
-	stdout, err := cmd.StdoutPipe()
-	if err != nil {
-		color.Red("Error stdin pipe")
-		log.Fatal(err)
+    stdout, err := cmd.StdoutPipe()
+    if err != nil {
+        color.Red("Error stdin pipe")
+        log.Fatal(err)
 
-		fmt.Println(err)
-		return
-	}
-	stderr, err := cmd.StderrPipe()
-	if err != nil {
-		color.Red("Error getting stderr pipe")
-		log.Fatal(err)
+        fmt.Println(err)
+        return
+    }
+    stderr, err := cmd.StderrPipe()
+    if err != nil {
+        color.Red("Error getting stderr pipe")
+        log.Fatal(err)
 
-		fmt.Println(err)
-		return
-	}
+        fmt.Println(err)
+        return
+    }
 
-	/* starting command & storing outputs in pipes */
+    /* starting command & storing outputs in pipes */
 
-	if err := cmd.Start(); err != nil {
-		color.Red("Error starting command")
-		log.Fatal(err)
-	}
-	go io.Copy(os.Stdout, stdout)
-	go io.Copy(os.Stderr, stderr)
+    if err := cmd.Start(); err != nil {
+        color.Red("Error starting command")
+        log.Fatal(err)
+    }
+    go io.Copy(os.Stdout, stdout)
+    go io.Copy(os.Stderr, stderr)
 
-	/* read input with bufio */
+    /* read input with bufio */
 
-	reader := bufio.NewReader(os.Stdin)
-	for {
-		char, _, err := reader.ReadRune()
-		if err != nil {
-			color.Red("Error reading user input")
-			log.Fatal(err)
-		}
-		if char == ' ' {
-			/* detaching from process */
-			cmd.Process.Release()
-			return
-		}
+    reader := bufio.NewReader(os.Stdin)
+    for {
+        char, _, err := reader.ReadRune()
+        if err != nil {
+            color.Red("Error reading user input")
+            log.Fatal(err)
+        }
+        if char == ' ' {
+            /* detaching from process */
+            cmd.Process.Release()
+            return
+        }
 
-		color.Green("[+] Success!")
-	}
+        stdout.Close()
+        stderr.Close()
+
+        color.Green("[+] Success!")
+    }
 }
