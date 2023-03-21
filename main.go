80ea7759e172373f510e2345d4b51b5ad7b85ba2
winds0r <wontsleep@protonmail.com>
2023-03-21 16:55:56 +0900

    fix(commands): using docker compose instead of docker-compose

--- main.go
+++ main.go
@@ -35,7 +35,7 @@ var (
    There are 5 commands that are supported by zru:
 
    - release: creates a branch /release from master, & pushes it to a new origin for software releases
-   - deploy: renames URL in relevant places & deploys the application through docker-compose
+   - deploy: renames URL in relevant places & deploys the application through docker compose
    - certs: initiates ssl certficates for the application
    - clone: git clones a bare repository
    - nuke: destroys an infrastructure, and resets it
@@ -75,7 +75,7 @@ func main() {
 	var deployCmd = &cobra.Command{
 		Use:     "deploy",
 		Short:   "deploys the Zenith timesheet tool",
-		Long:    "wraps around docker-compose to deploy the various services",
+		Long:    "wraps around docker compose to deploy the various services",
 		Version: version,
 		Args:    cobra.MinimumNArgs(0),
 		Run: func(cmd *cobra.Command, args []string) {
@@ -88,15 +88,15 @@ func main() {
 
 			d.SearchAndReplace(oldUrl, newUrl, repositoryPath)
 
-			command = exec.Command("docker-compose", "down")
-			c.RunHeadless(command, "docker-compose down", repositoryPath)
+			command = exec.Command("docker compose", "down")
+			c.RunHeadless(command, "docker compose down", repositoryPath)
 
             /* Fixes timeout on build on QA vps: https://stackoverflow.com/a/69432587 */
             os.Setenv("DOCKER_BUILDKIT", "0")
             os.Setenv("COMPOSE_DOCKER_CLI_BUILD", "0")
 
-			command = exec.Command("docker-compose", "up", "--build", "-d")
-			c.Run(command, "docker-compose up", repositoryPath)
+			command = exec.Command("docker compose", "up", "--build", "-d")
+			c.Run(command, "docker compose up", repositoryPath)
 
 		},
 	}
@@ -134,36 +134,36 @@ func main() {
 			}
 
 			entrypoint := fmt.Sprintf("openssl req -x509 -nodes -newkey rsa:4096 -days 1 -keyout '%s' -out '%s' -subj '/CN=localhost'", privPath, fullchainPath)
-			toRun := exec.Command("docker-compose", "run", "--rm", "--entrypoint", entrypoint, "certbot")
-			c.RunHeadless(toRun, "docker-compose run create key", repositoryPath)
+			toRun := exec.Command("docker compose", "run", "--rm", "--entrypoint", entrypoint, "certbot")
+			c.RunHeadless(toRun, "docker compose run create key", repositoryPath)
 
 			color.Green("Booting up nginx")
-			toRun = exec.Command("docker-compose", "up", "--force-recreate", "-d", "nginx")
-			c.RunHeadless(toRun, "docker-compose up nginx -d", repositoryPath)
+			toRun = exec.Command("docker compose", "up", "--force-recreate", "-d", "nginx")
+			c.RunHeadless(toRun, "docker compose up nginx -d", repositoryPath)
 
 			color.Green("Deleting dummy certificates")
 			entrypoint = fmt.Sprintf("rm -Rf /etc/letsencrypt/live/%s && rm -Rf /etc/letsencrypt/archive/%s && rm -Rf /etc/letsencrypt/renewal/%s.conf", domain, domain, domain)
-			toRun = exec.Command("docker-compose", "run", "--rm", "--entrypoint", entrypoint, "certbot")
-			c.RunHeadless(toRun, "docker-compose rm certs", repositoryPath)
+			toRun = exec.Command("docker compose", "run", "--rm", "--entrypoint", entrypoint, "certbot")
+			c.RunHeadless(toRun, "docker compose rm certs", repositoryPath)
 
 			color.Green("Requesting real certificates")
 			entrypoint = fmt.Sprintf("certbot certonly --webroot -w /var/www/certbot --email %s -d %s --rsa-key-size 4096 --agree-tos --force-renewal --non-interactive", email, domain)
-			toRun = exec.Command("docker-compose", "run", "--rm", "--entrypoint", entrypoint, "certbot")
-			c.RunHeadless(toRun, "docker-compose run certbot", repositoryPath)
+			toRun = exec.Command("docker compose", "run", "--rm", "--entrypoint", entrypoint, "certbot")
+			c.RunHeadless(toRun, "docker compose run certbot", repositoryPath)
 
 			color.Green("Reloading nginx")
-			toRun = exec.Command("docker-compose", "exec", "nginx", "nginx", "-s", "reload")
-			c.RunHeadless(toRun, "docker-compose exec nginx nginx -s reload", repositoryPath)
+			toRun = exec.Command("docker compose", "exec", "nginx", "nginx", "-s", "reload")
+			c.RunHeadless(toRun, "docker compose exec nginx nginx -s reload", repositoryPath)
 
 			color.Green("SSL setup done -- rebuilding application")
-			toRun = exec.Command("docker-compose", "down")
-			c.RunHeadless(toRun, "docker-compose down --rmi local", repositoryPath)
-			toRun = exec.Command("docker-compose", "up", "--build", "-d")
-			c.Run(toRun, "docker-compose up --build -d", repositoryPath)
+			toRun = exec.Command("docker compose", "down")
+			c.RunHeadless(toRun, "docker compose down --rmi local", repositoryPath)
+			toRun = exec.Command("docker compose", "up", "--build", "-d")
+			c.Run(toRun, "docker compose up --build -d", repositoryPath)
 
             color.Green("\nDeployment done\n\tCurrent logs:\n")
-            toRun = exec.Command("docker-compose", "logs", "-f", "-t")
-            c.Run(toRun, "docker-compose logs -f -t", repositoryPath)
+            toRun = exec.Command("docker compose", "logs", "-f", "-t")
+            c.Run(toRun, "docker compose logs -f -t", repositoryPath)
 		},
 	}
 
@@ -214,8 +214,8 @@ func main() {
 
 			color.Green("Stopping and removing all docker images")
 
-            toRun := exec.Command("docker-compose", "down")
-			c.RunHeadless(toRun, "docker-compose down", repositoryPath)
+            toRun := exec.Command("docker compose", "down")
+			c.RunHeadless(toRun, "docker compose down", repositoryPath)
             toRun = exec.Command("docker", "image", "prune", "-a", "f")
 			c.RunHeadless(toRun, "docker image prune -a -f", repositoryPath)
 
